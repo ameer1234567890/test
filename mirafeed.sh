3326a678575697325fa5e09d911d3a870c569019
Ameer Dawood <ameer1234567890@gmail.com>
2018-10-04 02:25:38 +0500

    Feed fixed

--- mirafeed.sh
+++ mirafeed.sh
@@ -3,5 +3,6 @@ while true; do
   ./mirafeed
   git add .
   git diff-index --quiet HEAD -- || git commit --no-gpg-sign -m 'Feed fixed' && git push
+  echo "Sleeping for half an hour"
   sleep 10
 done
