63258d3fe2757d7b914da1496d58bba691558967
Ameer Dawood <ameer1234567890@gmail.com>
2018-10-08 22:44:53 +0500

    Add notifications for feed validation

--- mirafeed.sh
+++ mirafeed.sh
@@ -4,10 +4,11 @@ while true; do
   git add .
   git diff-index --quiet HEAD -- || git commit --no-gpg-sign -m 'Feed fixed' && git push
   result="$(./feedvalidator/src/demo.py https://ameer.io/test/FixedFeed.rss | grep -v Validating | grep -v guid)"
-  if [ "$result" == "" ]; then
+  if [ "$result" != "" ]; then
     echo "Errors detected in feed!"
-    curl "https://maker.ifttt.com/trigger/mirafeed_error/with/key/hAVKuiLxTZFFNyiGtd1FubyVsOwTOHzWzJocBA0dCJs?value1=$result"
+    curl --data-urlencode "value1=$result" https://maker.ifttt.com/trigger/mirafeed_error/with/key/hAVKuiLxTZFFNyiGtd1FubyVsOwTOHzWzJocBA0dCJs
   fi
+  echo ""
   echo "Sleeping for half an hour"
   sleep 30m
 done
